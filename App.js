cb4b598ce7eb6ae6b5ba30171fc3e8ee65a79f30
Chreyd <chreydmapeba@gmail.com>
2023-03-02 17:19:46 +0100

    Test sur fonction

--- App.js
+++ App.js
@@ -1,27 +1,42 @@
-import React, {useState} from "react";
-import { Button, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
 
-export default function App() {
-  const [count, setCount] = useState(0);
+const App= ()=> {
+
+  const getName = (firsName, secondName, tirdName, lastName )=>{
+    return firsName +' '+ secondName+' '+ tirdName+ ' '+ lastName
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.counter}> {count} </Text>
-      <Button title="+1" onPress={()=>setCount(count +1)} />
-      <Button color="#840000" title="-1" onPress={()=>setCount(count -1)} />
-    </View>                                                                                                                                                                                                                                                                                                                                                                     
+      <Text style={styles.text}>je m'appelle {getName('LOLANGOY', 'MAPEBA', 'Fleudry','Chreyd')} </Text>
+
+    </View>
   );
 }
+const styles=StyleSheet.create({
+    container:{
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: '#000'
+    },
+    text:{
+      fontSize: 40,
+      color: '#FFF'
+    },
+    text2:{
+      fontSize: 40,
+      fontWeight: 'bold',
+      marginBottom: 2,
+      color: '#FFF'
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#841584',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  counter:{
-    fontSize: 120,
-  },
+    },
+    text3:{
+      fontSize: 40,
+      color: '#FFF'
 
+    }
+})
 
-});
+export default App;
