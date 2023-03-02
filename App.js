cc3fef43d96c2f125e26b5428ad00688271ce221
Chreyd <chreydmapeba@gmail.com>
2023-03-02 20:05:41 +0100

    TextInput et onChange

--- App.js
+++ App.js
@@ -1,53 +1,24 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, TextInput } from "react-native";
 
 const App = () => {
-  const [count, setCount] = useState(0);
-  const [name, setName] = useState("Chreyd");
-
-  const obj = {
-    name: "Spiderman",
-    age: 30,
-  };
-  const handlePress = () => {
-    setInfos({
-      name: "Batman",
-      age: 37,
-    });
-  };
-  const [infos, setInfos] = useState(obj);
-
-  const change = (arg) => {
-    setName(arg);
-  };
+  const [name, setName] = useState();
+  const [age, setAge] = useState();
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text3}>{infos.name} </Text>
-      <Text style={styles.text3}>{infos.age} </Text>
-      <Button
-        title="Cliquez ici"
-        onPress={() =>
-          setInfos({
-            name: "Batman",
-            age: 37,
-          })
-        }
+      <Text style={styles.text}>Nom: {name} </Text>
+      <TextInput
+        style={styles.textInput}
+        onChangeText={(val) => setName(val)}
+        placeholder="Indiquez votre nom"
       />
-
-      <Text style={styles.text}>Je suis {name}</Text>
-      <Button title="Clicquez ici" onPress={() => change("Fleudry")} />
-      <Text style={styles.text}>{count}</Text>
-      <Button
-        title="Augmanter de 1"
-        disabled={false}
-        onPress={() => setCount(count + 1)}
-      />
-      <Button
-        title="Diminuer de 1"
-        color="red"
-        onPress={() => setCount(count - 1)}
-        disabled={count == 0 ? true : false}
+      <Text style={styles.text2}>Age: {age} </Text>
+      <TextInput
+        style={styles.textInput}
+        onChangeText={(val) => setAge(val)}
+        placeholder="Indiquez votre age"
+        keyboardType="numeric"
       />
     </View>
   );
@@ -65,13 +36,13 @@ const styles = StyleSheet.create({
   },
   text2: {
     fontSize: 20,
-    fontWeight: "bold",
-    marginBottom: 2,
-    color: "#FFF",
-  },
-  text3: {
-    fontSize: 20,
     color: "#0F0",
+  },
+  textInput: {
+    backgroundColor: "#fff",
+    width: "80%",
+    height: 30,
+    borderRadius: 5,
   },
 });
 
