2916217a026f1ac2a5489ec5b20a455427137845
Chreyd <chreydmapeba@gmail.com>
2023-03-02 21:36:11 +0100

    map & ScrollView

--- App.js
+++ App.js
@@ -1,34 +1,67 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, Button, TextInput } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  TextInput,
+  ScrollView,
+} from "react-native";
 
 const App = () => {
-  const [name, setName] = useState();
-  const [age, setAge] = useState();
+  const obj = [
+    { id: 1, name: "aaaaa", age: 25 },
+    { id: 2, name: "bbbb", age: 23 },
+    { id: 3, name: "cccc", age: 20 },
+    { id: 4, name: "dddd", age: 15 },
+    { id: 5, name: "eeee", age: 5 },
+    { id: 6, name: "aaaaa", age: 25 },
+    { id: 7, name: "bbbb", age: 23 },
+    { id: 8, name: "cccc", age: 20 },
+    { id: 9, name: "dddd", age: 15 },
+    { id: 10, name: "eeee", age: 5 },
+    { id: 11, name: "aaaaa", age: 25 },
+    { id: 12, name: "bbbb", age: 23 },
+    { id: 13, name: "cccc", age: 20 },
+    { id: 14, name: "dddd", age: 15 },
+    { id: 15, name: "eeee", age: 5 },
+    { id: 16, name: "aaaaa", age: 25 },
+    { id: 17, name: "bbbb", age: 23 },
+    { id: 18, name: "cccc", age: 20 },
+    { id: 19, name: "dddd", age: 15 },
+    { id: 20, name: "eeee", age: 5 },
+  ];
+  const [family, setFamily] = useState(obj);
+
+  const display = family.map((x) => {
+    return (
+      <View key={x.id} style={styles.display}>
+        <Text style={styles.text}>{x.name} </Text>
+        <Text style={styles.text}>{x.age} </Text>
+      </View>
+    );
+  });
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Nom: {name} </Text>
-      <TextInput
-        style={styles.textInput}
-        onChangeText={(val) => setName(val)}
-        placeholder="Indiquez votre nom"
-      />
-      <Text style={styles.text2}>Age: {age} </Text>
-      <TextInput
-        style={styles.textInput}
-        onChangeText={(val) => setAge(val)}
-        placeholder="Indiquez votre age"
-        keyboardType="numeric"
-      />
+      <ScrollView>{display}</ScrollView>
     </View>
   );
 };
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    // flex: 1,
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "#700",
+  },
+  display: {
+    flexDirection: "row",
+    backgroundColor: "#aba",
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 20,
   },
   text: {
     fontSize: 20,
@@ -40,7 +73,6 @@ const styles = StyleSheet.create({
   },
   textInput: {
     backgroundColor: "#fff",
-    width: "80%",
     height: 30,
     borderRadius: 5,
   },
