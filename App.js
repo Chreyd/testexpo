541e951f56cd7b1b2511fcbf4db03c1688be04f4
Chreyd <chreydmapeba@gmail.com>
2023-03-02 16:23:25 +0100

    Test sur expo 1

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from "react";
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Text style={styles.counter}> {count} </Text>
+      <Button title="+1" onPress={()=>setCount(count +1)} />
+      <Button color="#840000" title="-1" onPress={()=>setCount(count -1)} />
+    </View>                                                                                                                                                                                                                                                                                                                                                                     
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#841584',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  counter:{
+    fontSize: 120,
+  },
+
+
 });
