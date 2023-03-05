eae4fa436f50e767603d616dd82bfd1cc9bc3941
Chreyd <chreydmapeba@gmail.com>
2023-03-05 18:36:22 +0100

    Presseble et les Touchable

--- components/Product.js
+++ components/Product.js
@@ -1,25 +1,43 @@
-import { StyleSheet, Text, View, FlatList } from 'react-native'
-import React from 'react'
-
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  TouchableHighlight,
+  Pressable,
+} from "react-native";
+import React from "react";
 
 const Product = (props) => {
   return (
-    <View style={styles.items}>
+    <Pressable
+      onLongPress={() => console.log(props.name)}
+      style={({ pressed }) => [
+        {
+          backgroundColor: pressed ? "#00F" : "#F00",
+        },
+      ]}
+      delayLongPress={4000}
+    >
+      <View style={styles.items}>
         <Text style={styles.element}>{props.name}</Text>
-  </View>
-  )
-}
+      </View>
+    </Pressable>
+  );
+};
 
 const styles = StyleSheet.create({
-    items: {
-      marginTop: 10,
-    },
-    element: {
-      backgroundColor: "#ffb6c1",
-      padding: 20,
-      fontSize: 17,
-      marginVertical: 6,
-    },
-  });
+  items: {
+    marginTop: 10,
+  },
+  element: {
+    backgroundColor: "#ffb6c1",
+    padding: 20,
+    fontSize: 17,
+    marginVertical: 6,
+  },
+});
 
-export default Product
+export default Product;
